c8fc643ad0ff03d4595ea90306866a6bd621b63c
Chenxing Dong <dchx@users.noreply.github.com>
2020-04-09 20:29:07 +0800

    pickle add save

--- pickle23.py
+++ pickle23.py
@@ -1,7 +1,16 @@
 '''
-Handle pickle load for python2 and python3
+Handle pickle load/dump for python2 and python3
 '''
 import pickle, gzip, sys
+
+def pkdump(data, pfile, verbose=True):
+        '''
+        Save *.pickle files
+        '''
+        with open(pfile,'wb') as f:
+                if sys.version_info.major == 2: pickle.dump(data, f)
+                if sys.version_info.major == 3: pickle.dump(data, f, protocol=2)
+        if verbose: print('Saved:%s'%pfile)
 
 def pkload(pfile, verbose=True):
         '''
@@ -13,6 +22,15 @@ def pkload(pfile, verbose=True):
                 with open(pfile, 'rb') as f: results = pickle.load(f, encoding='latin1')
         if verbose: print('Loaded:%s'%pfile)
         return results
+
+def pkdumpgzip(data, pfile, verbose=True):
+        '''
+        Save *.pzip files
+        '''
+        with gzip.open(pfile,'wb') as f:
+                if sys.version_info.major == 2: pickle.dump(data, f)
+                if sys.version_info.major == 3: pickle.dump(data, f, protocol=2)
+        if verbose: print('Saved:%s'%pfile)
 
 def pkloadgzip(pfile, verbose=True):
         '''
